edca0e1ff28cd833863c1a74a45f0703511c5aee
progra33erJ <3affy.happy7@gmail.com>
2024-05-03 09:38:04 +0900

    例外をハンドリングする処理を追加

--- src/main/java/com/example/assessment/AssessmentMapper.java
+++ src/main/java/com/example/assessment/AssessmentMapper.java
@@ -4,9 +4,14 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface AssessmentMapper {
     @Select("SELECT * FROM assessments WHERE name LIKE CONCAT(#{prefix}, '%' )")
     List<Assessment> findByNameStartingWith(String prefix);
+
+
+    @Select("SELECT * FROM assessments WHERE id = #{id}")
+    Optional<Assessment> findByIdSearch(int id);
 }
---
+++ src/main/java/com/example/assessment/AssessmentService.java
@@ -0,0 +1,28 @@
+package com.example.assessment;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AssessmentService {
+    private final AssessmentMapper assessmentMapper;
+
+    public AssessmentService(AssessmentMapper assessmentMapper) {
+        this.assessmentMapper = assessmentMapper;
+    }
+
+    public List<Assessment> findByStartingWith(String startsWith) {
+        return assessmentMapper.findByNameStartingWith(startsWith);
+    }
+
+    public Assessment findById(int id) {
+        Optional<Assessment> user = assessmentMapper.findByIdSearch(id);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new ClientNotFoundException("Client with id " + id + " not found");
+        }
+    }
+}
